dfdf57e8f8de3529a50e18e78464f922624a9229
Franck <franckvienot7@gmail.com>
2024-03-10 22:34:34 +0100

    add style to the different part of the Jokes app

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,9 @@
-import type { MetaFunction } from "@remix-run/node"
+import { Link } from "@remix-run/react"
+import type { MetaFunction, LinksFunction } from "@remix-run/node"
+
+import stylesUrl from "../styles/index.css"
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesUrl }]
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,26 +14,19 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      hello Index Route
-      <h1>Welcome to My Remix app</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
